6e83b6131a89ecd9bf90afd158f4ecd136d0ce65
Johan Back <johan.backs@gmail.com>
2020-07-28 11:31:26 -0300

    pag cadastro

--- src/components/Menu/index.js
+++ src/components/Menu/index.js
@@ -1,4 +1,5 @@
 import React from "react";
+import {Link} from 'react-router-dom';
 import Logo from "../../assets/img/Logo.png";
 import "./Menu.css";
 import Button from "../Button"
@@ -6,10 +7,11 @@ import Button from "../Button"
 function Menu() {
   return (
   <nav className="Menu">
-    <a href="/">
-   <img className="Logo" src={Logo} alt="ReactFlix"/>
-   </a>
-   <Button as="a" className="ButtonLink" href="/">
+    <Link to="/">
+      <img className="Logo" src={Logo} alt="ReactFlix"/>
+   </Link>
+
+    <Button as={Link} className="ButtonLink" to="/cadastro/video">
      Novo Vídeo
     </Button>
    </nav>
